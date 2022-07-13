d9f792f1049bd24c5d73e802599e2f889727f69b
h359hira <108774399+h359hira@users.noreply.github.com>
2022-07-13 12:13:39 +0900

    Add files via upload

---
+++ a.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+<?php 
+  foreach(file('/usr/share/dict/linux.words') as $line){
+     if(strpos("$line", "abc") == FALSE){
+     }
+     else{
+        echo "$line";
+     }
+  } 
+?>
